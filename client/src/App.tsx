950fe540d79ef5b0254b15db7ccd26de8662d834
JoaoVictorFerrer <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-08-16 12:15:14 +0200

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,27 @@
 
-import './App.css'
+import performRequest from './services/Network/DataFetcher'
+import { HTTPMethod } from './services/Network/Entities'
+import { ServerEndpoints } from './services/Network/ServerEndpoints'
 
 function App() {
 
+  const checkServices = async () => {
+
+    const body = {
+        Email: "some@email.com",
+        password: "12345678"
+    }
+
+    const response = await performRequest(HTTPMethod.POST, ServerEndpoints.login(),body)
+    console.log('esto es response',response)
+  }
+
   return (
     <>
-        <h1>hola</h1>
+      <div className='flex justify-center items-center flex-col size-full bg-slate-600 m-auto '>
+        <h1 className='text-6xl'>Hola mundo</h1>
+        <button onClick={checkServices}>click me</button>
+        </div>
     </>
   )
 }
