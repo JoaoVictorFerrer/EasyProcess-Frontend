bbbc8bb15b604c32496d1af23e419e70fd7216be
JoaoVictorFerrer <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-08-18 21:53:59 +0200

    installed the tailwind dependencies and resolved the type incompatibility problems with said dependency

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@material-tailwind/react"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world! que tailwind
+        </h1>
+        <Button>Button</Button>;
+      </div>
+    </>
+  )
+}
+
+export default App
